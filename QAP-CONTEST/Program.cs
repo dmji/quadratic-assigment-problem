e3d8ffc09a4eb4e480f8f1e7f5fcfed7d90032c2
daimonji <leyshkinanton@gmail.com>
2020-08-07 12:40:26 +0300

    ux

---
+++ QAP-CONTEST/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using QAPenviron;
+
+namespace QAP_CONTEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] src = { "..\\..\\..\\..\\QAP-CONTEST\\contest\\ex1.txt" }; 
+            QAPenviron.Program.Main(src);
+        }
+    }
+}
--- QAP-FULLFORCE/Program.cs
+++ QAP-FULLFORCE/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using QAPenviron;
 
@@ -7,9 +8,13 @@ namespace QAPenviron
 {
     public class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
-            QAP_FULLFORCE ss = new QAP_FULLFORCE(new Info("..\\..\\..\\..\\QAP-CONTEST\\ex1.txt"));
+            QAP_FULLFORCE ss;
+            if(args.Length >0)
+                ss= new QAP_FULLFORCE(new Info(args[0]));
+            else
+                ss= new QAP_FULLFORCE(new Info("..\\..\\..\\..\\QAP-CONTEST\\ex1.txt"));
             ss.Start();
             ss.ShowInConsole();
             ss.StartMT();
