c7923aad29a771e5c1df31e0e66d353a15a5d5c5
daimonji <leyshkinanton@gmail.com>
2021-05-16 15:18:57 +0300

    runner

--- QAP-Solution/XML-Runner/RunnerTwice.cs
+++ QAP-Solution/XML-Runner/RunnerTwice.cs
@@ -1,5 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Solution;
+using TestSystem;
 
 namespace XML_Runner
 {
@@ -9,102 +9,102 @@ namespace XML_Runner
         [TestMethod]
         public void _all()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_all.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_all.xml", 2);
         }
         [TestMethod]
         public void _chr()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_chr.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_chr.xml", 2);
         }
         [TestMethod]
         public void _els()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_els.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_els.xml", 2);
         }
         [TestMethod]
         public void _esc()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_esc.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_esc.xml", 2);
         }
         [TestMethod]
         public void _had()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_had.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_had.xml", 2);
         }
         [TestMethod]
         public void _kra()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_kra.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_kra.xml", 2);
         }
         [TestMethod]
         public void _lipa()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_lipa.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_lipa.xml", 2);
         }
         [TestMethod]
         public void _nug()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_nug.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_nug.xml", 2);
         }
         [TestMethod]
         public void _rou()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_rou.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_rou.xml", 2);
         }
         [TestMethod]
         public void _sko()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_sko.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_sko.xml", 2);
         }
         [TestMethod]
         public void _scr()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_scr.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_scr.xml", 2);
         }
         [TestMethod]
         public void _ste()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_ste.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_ste.xml", 2);
         }
         [TestMethod]
         public void _tai()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_tai.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_tai.xml", 2);
         }
         [TestMethod]
         public void _tho()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_tho.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_tho.xml", 2);
         }
         [TestMethod]
         public void _wil()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_wil.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_wil.xml", 2);
         }
         [TestMethod]
         public void _bur()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_bur.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_bur.xml", 2);
         }
         [TestMethod]
         public void _eva_test_little()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_eva_test_little.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_eva_test_little.xml", 2);
         }
         [TestMethod]
         public void _eva_test_big()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_eva_test_big.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\_eva_test_big.xml", 2);
         }
         [TestMethod]
         public void badPROB()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\badPROB.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\badPROB.xml", 2);
         }
         [TestMethod]
         public void goodPROB()
         {
-            TestSystem.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\goodPROB.xml", 2);
+            STestStarter.StartTestEvolution("C:\\Users\\leysh\\source\\repos\\dmji\\quadratic-assigment-problem\\QAP-Solution\\Contest\\goodPROB.xml", 2);
         }
     }
 }
